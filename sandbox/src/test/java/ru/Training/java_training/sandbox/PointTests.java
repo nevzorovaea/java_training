e7143e9c45a3867f11772ae6efcc493ab4005bf7
nevzorovaea <nevzorovaea7@gmail.com>
2020-10-25 22:16:38 +0300

    Подключен тестовый фреймворк и сделаны первые тесты для класса Point

---
+++ sandbox/src/test/java/ru/Training/java_training/sandbox/PointTests.java
@@ -0,0 +1,33 @@
+package ru.Training.java_training.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static ru.Training.java_training.sandbox.Distance.distance;
+
+public class PointTests {
+  public double distance;
+
+  @Test
+  public void testPoint (){
+
+    Point P1 = new Point(3, 4);
+    Point P2 = new Point(5, 6);
+
+    Assert.assertEquals (distance(P1, P2), 2.8284271247461903);
+
+  }
+
+  @Test
+  public void testPointNegative (){
+
+    Point P1 = new Point(3, 4);
+    Point P2 = new Point(5, 6);
+
+    Assert.assertNotEquals(distance(P1, P2), 10);
+
+  }
+
+}
+
+
