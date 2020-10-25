7cefa4e65126ecfe89e963c5b751d88d45c37d6b
nevzorovaea <nevzorovaea7@gmail.com>
2020-10-25 13:33:32 +0300

    Методы: функции, ассоциированные с объектом

--- sandbox/src/main/java/ru/Training/java_training/sandbox/MyFirstProgramm.java
+++ sandbox/src/main/java/ru/Training/java_training/sandbox/MyFirstProgramm.java
@@ -6,12 +6,11 @@ public static void main(String[] args) {
  hello ("user");
  hello ("Alexei");
  hello("Vladimir");
-
  Square s = new Square(5);
- System.out.println("Площадь квадрата со стороной " + s.l + " = " + area (s));
+ System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area ());
 
  Rectangle r = new Rectangle(4, 6);
- System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + area(r));
+ System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
 
  }
 
@@ -20,10 +19,4 @@ public static void main(String[] args) {
    System.out.println("Hello," + somebody + "!");
  }
 
- public static double area (Square s) {
- return s.l * s.l;
- }
-public static double area (Rectangle r) {
- return r.a * r.b;
-}
 }
--- sandbox/src/main/java/ru/Training/java_training/sandbox/Square.java
+++ sandbox/src/main/java/ru/Training/java_training/sandbox/Square.java
@@ -5,4 +5,8 @@ public class Square {
   public Square(double l) {
     this.l = l;
   }
+
+  public double area () {
+    return this.l * this.l;
+  }
 }
