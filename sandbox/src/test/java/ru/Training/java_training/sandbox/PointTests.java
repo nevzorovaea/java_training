f4caeb733416f734af7361a4d90f15b028d71eff
nevzorovaea <nevzorovaea7@gmail.com>
2020-10-26 14:46:00 +0300

    Добавлены тесты измерения расстояния через метод класса Point

--- sandbox/src/test/java/ru/Training/java_training/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/Training/java_training/sandbox/PointTests.java
@@ -9,17 +9,17 @@ public class PointTests {
   public double distance;
 
   @Test
-  public void testPoint (){
+  public void testPoint() {
 
     Point P1 = new Point(3, 4);
     Point P2 = new Point(5, 6);
 
-    Assert.assertEquals (distance(P1, P2), 2.8284271247461903);
+    Assert.assertEquals(distance(P1, P2), 2.8284271247461903);
 
   }
 
   @Test
-  public void testPointNegative (){
+  public void testPointNegative() {
 
     Point P1 = new Point(3, 4);
     Point P2 = new Point(5, 6);
@@ -28,6 +28,24 @@ public class PointTests {
 
   }
 
+  @Test
+  public void testPointNegative2() {
+
+    Point P1 = new Point(3, 4);
+    Point P2 = new Point(5, 6);
+
+    Assert.assertNotEquals(P1.distance(P2), 10);
+
+  }
+
+  @Test
+  public void testPoint2() {
+
+    Point P1 = new Point(3, 4);
+    Point P2 = new Point(5, 6);
+
+    Assert.assertEquals(P1.distance(P2), 2.8284271247461903);
+  }
 }
 
 
