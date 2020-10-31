ed257774639352dd7ab4c3702e114bb6c2c58a68
nevzorovaea <nevzorovaea7@gmail.com>
2020-10-31 13:23:21 +0300

    Инициализация фикстур и зачистка

--- tests-web-addressbook/src/test/java/ru/training/java_training/addressbook/CreationNewContact.java
+++ tests-web-addressbook/src/test/java/ru/training/java_training/addressbook/CreationNewContact.java
@@ -13,10 +13,6 @@ public class CreationNewContact {
   public void setUp() throws Exception {
     wd = new FirefoxDriver();
    wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-  }
-
-  @Test
-  public void testCreationNewContact() throws Exception {
     wd.get("http://localhost/addressbook/");
     wd.findElement(By.name("user")).click();
     wd.findElement(By.name("user")).clear();
@@ -24,7 +20,11 @@ public class CreationNewContact {
     wd.findElement(By.name("pass")).clear();
     wd.findElement(By.name("pass")).sendKeys("secret");
     wd.findElement(By.xpath("//input[@value='Login']")).click();
-    wd.findElement(By.id("header")).click();
+  }
+
+  @Test
+  public void testCreationNewContact() throws Exception {
+       wd.findElement(By.id("header")).click();
     wd.findElement(By.linkText("add new")).click();
     wd.findElement(By.name("firstname")).click();
     wd.findElement(By.name("firstname")).clear();
