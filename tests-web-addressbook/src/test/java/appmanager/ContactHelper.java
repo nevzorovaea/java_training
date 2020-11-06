67254f928edd2ed326851c4941b1afe93b3bb070
nevzorovaea <nevzorovaea7@gmail.com>
2020-11-06 21:54:05 +0300

    Реализована возможность запуска тестов в разных браузерах (Firefox, Chrome, IE)

--- tests-web-addressbook/src/test/java/appmanager/ContactHelper.java
+++ tests-web-addressbook/src/test/java/appmanager/ContactHelper.java
@@ -1,13 +1,14 @@
 package appmanager;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 import tests.NewContact;
 
 public class ContactHelper extends HelperBase {
 
-  public ContactHelper(FirefoxDriver wd) {
+  public ContactHelper(WebDriver wd) {
 
     super(wd);
   }
--- tests-web-addressbook/src/test/java/tests/TestBase.java
+++ tests-web-addressbook/src/test/java/tests/TestBase.java
@@ -2,13 +2,16 @@ package tests;
 
 import appmanager.ApplicationManager;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
-public class TestBase extends ApplicationManager{
+public class TestBase {
 
-  protected final ApplicationManager app = new ApplicationManager();
-  private WebDriver wd;
+  protected final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
+
+  public TestBase() {
+  }
 
   @BeforeMethod(alwaysRun = true)
   public void setUp() {
