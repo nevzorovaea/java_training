0cb20a6dd769728302c2a3e0e97a8f9dac047b3f
nevzorovaea <nevzorovaea7@gmail.com>
2020-11-08 12:45:50 +0300

    Оптимизированы переходы между страницами

--- tests-web-addressbook/src/test/java/appmanager/NavigationHelper.java
+++ tests-web-addressbook/src/test/java/appmanager/NavigationHelper.java
@@ -2,7 +2,6 @@ package appmanager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class NavigationHelper extends HelperBase{
 
@@ -11,11 +10,18 @@ public class NavigationHelper extends HelperBase{
   }
 
   public void gotoGroupPage() {
-
+        if (isElementPresent(By.tagName("h1"))
+            && wd.findElement(By.tagName("h1")).getText().equals("Groups")
+            && isElementPresent(By.name("new")))  {
+      return;
+            }
     click(By.linkText("groups"));
   }
 
   public void gotoHomePage() {
+    if(isElementPresent(By.id("maintable"))){
+      return;
+    }
     click(By.linkText("home"));
   }
 
