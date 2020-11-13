4b461abcc4226e40c3a187e925e7d7cf0580f530
nevzorovaea <nevzorovaea7@gmail.com>
2020-11-13 20:38:57 +0300

    Сравнение коллекций

--- tests-web-addressbook/src/test/java/tests/GroupDeletionTests.java
+++ tests-web-addressbook/src/test/java/tests/GroupDeletionTests.java
@@ -9,16 +9,19 @@ import java.util.List;
 public class GroupDeletionTests extends TestBase {
 
   @Test
-  public void testGroupDeletion()  {
+  public void testGroupDeletion() {
     app.getNavigationHelper().gotoGroupPage();
-    if (! app.getGroupHelper().isThereAGroup()) {
-         app.getGroupHelper().createGroup(new GroupData("test1", null, null));
-      }
+    if (!app.getGroupHelper().isThereAGroup()) {
+      app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+    }
     List<GroupData> before = app.getGroupHelper().getGroupList();
     app.getGroupHelper().selectGroup(before.size() - 1);
-      app.getGroupHelper().deleteSelectedGroups();
-      app.returnToGroupPage();
+    app.getGroupHelper().deleteSelectedGroups();
+    app.returnToGroupPage();
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size() - 1);
-  }
+
+    before.remove(before.size() - 1);
+      Assert.assertEquals(before, after);
+     }
 }
