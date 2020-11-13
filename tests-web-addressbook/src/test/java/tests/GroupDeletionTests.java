5d2539d92d03efdbc67129ab10298956a383463e
nevzorovaea <nevzorovaea7@gmail.com>
2020-11-13 16:05:55 +0300

    Количество элементов в коллекции

--- tests-web-addressbook/src/test/java/tests/GroupDeletionTests.java
+++ tests-web-addressbook/src/test/java/tests/GroupDeletionTests.java
@@ -1,21 +1,22 @@
 package tests;
 
 import model.GroupData;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class GroupDeletionTests extends TestBase {
 
   @Test
   public void testGroupDeletion()  {
-
-      app.getNavigationHelper().gotoGroupPage();
-      if (! app.getGroupHelper().isThereAGroup()) {
+    app.getNavigationHelper().gotoGroupPage();
+    int before = app.getGroupHelper().getGroupCount();
+    if (! app.getGroupHelper().isThereAGroup()) {
          app.getGroupHelper().createGroup(new GroupData("test1", null, null));
       }
       app.getGroupHelper().selectGroup();
       app.getGroupHelper().deleteSelectedGroups();
       app.returnToGroupPage();
-
-      }
-
+    int after = app.getGroupHelper().getGroupCount();
+    Assert.assertEquals(after, before - 1);
+  }
 }
