937dafa3f464f96cff57c89a1d7297b670144931
nevzorovaea <nevzorovaea7@gmail.com>
2020-11-14 13:21:36 +0300

    Сортировка списков

--- tests-web-addressbook/src/test/java/model/GroupData.java
+++ tests-web-addressbook/src/test/java/model/GroupData.java
@@ -10,7 +10,7 @@ public class GroupData {
   private final String footer;
 
   public GroupData(String name, String header, String footer) {
-    this.id = 0;
+    this.id = Integer.MAX_VALUE;
     this.name = name;
     this.header = header;
     this.footer = footer;
@@ -21,6 +21,11 @@ public class GroupData {
     this.name = name;
     this.header = header;
     this.footer = footer;
+  }
+
+    @Override
+  public int hashCode() {
+    return Objects.hash(name);
   }
 
   public void setId(int id) {
@@ -44,24 +49,18 @@ public class GroupData {
   }
 
   @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-    GroupData groupData = (GroupData) o;
-    return id == groupData.id &&
-            Objects.equals(name, groupData.name);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(id, name);
-  }
-
-  @Override
   public String toString() {
     return "GroupData{" +
             "id='" + id + '\'' +
             ", name='" + name + '\'' +
             '}';
   }
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    GroupData groupData = (GroupData) o;
+    return Objects.equals(name, groupData.name);
+  }
+
 }
--- tests-web-addressbook/src/test/java/tests/GroupModificationTests.java
+++ tests-web-addressbook/src/test/java/tests/GroupModificationTests.java
@@ -4,6 +4,8 @@ import model.GroupData;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -19,7 +21,7 @@ public class GroupModificationTests extends TestBase {
     List<GroupData> before = app.getGroupHelper().getGroupList();
     app.getGroupHelper().selectGroup(before.size() - 1);
     app.getGroupHelper().initGroupModification();
-    GroupData group = new GroupData(before.get(before.size() - 1).getId(),"test1", "test2", "test3");
+    GroupData group = new GroupData(before.get(before.size() - 1).getId(), "test1", "test2", "test3");
     app.getGroupHelper().fillGroupForm(group);
     app.getGroupHelper().submitGroupModification();
     app.getGroupHelper().returntoGroupPage();
@@ -28,6 +30,9 @@ public class GroupModificationTests extends TestBase {
 
     before.remove(before.size() - 1);
     before.add(group);
-    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
+    Comparator<? super GroupData> byId = (g1, g2)-> Integer.compare(g1.getId(), g2.getId());
+    before.sort(byId);
+    after.sort(byId);
+    Assert.assertEquals(before, after);
   }
 }
