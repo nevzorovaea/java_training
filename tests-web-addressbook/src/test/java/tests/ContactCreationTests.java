b34516ded259dabe7890ed70a76147f161a5f204
nevzorovaea <nevzorovaea7@gmail.com>
2020-12-01 14:33:28 +0300

    Загрузка тестовых данных из файла для Contact

--- tests-web-addressbook/src/test/java/tests/ContactCreationTests.java
+++ tests-web-addressbook/src/test/java/tests/ContactCreationTests.java
@@ -5,6 +5,7 @@ import model.Contacts;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -16,11 +17,15 @@ import static org.testng.Assert.assertEquals;
 public class ContactCreationTests extends TestBase {
 
   @DataProvider
-  public Iterator<Object[]> validContacts() {
+  public Iterator<Object[]> validContacts() throws IOException {
     List<Object[]> list = new ArrayList<Object[]>();
-    list.add(new Object[] {new ContactData().withFirstname("Anton1").withLastname("ivanov1").withAddress("Sovetskaya, dom 5").withEmail("test@mail").withMobile("556665")});
-    list.add(new Object[] {new ContactData().withFirstname("pavel2").withLastname("testov2").withAddress("teatralnaya, dom 21").withEmail("test2@mail").withMobile("9896512")});
-    list.add(new Object[] {new ContactData().withFirstname("lena3").withLastname("lenova3").withAddress("pervaya, dom 7").withEmail("tes3t@mail").withMobile("651623")});
+    BufferedReader reader = new BufferedReader(new FileReader("src/test/resources/contacts.csv"));
+    String line = reader.readLine();
+    while (line != null) {
+     String[] split = line.split(";");
+     list.add(new Object[] {new ContactData().withFirstname(split[0]).withLastname(split[1])});
+      line = reader.readLine();
+    }
     return list.iterator();
   }
 
@@ -28,7 +33,7 @@ public class ContactCreationTests extends TestBase {
   public void testCreationNewContact(ContactData contact) {
     app.goTo().gotohomePage();
     Contacts before = app.contact().all();
-    //File photo = new File("src/test/resources/zefir.png");     // фото
+    File photo = new File("src/test/resources/zefir.png");     // фото
     app.contact().create(contact);
     Contacts after = app.contact().all();
     assertEquals(app.contact().count(), before.size() + 1);
