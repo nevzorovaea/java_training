3d27ed8519bdc34e30b8bc095fead5e7aafcd525
nevzorovaea <nevzorovaea7@gmail.com>
2020-12-14 11:44:14 +0300

    Прямое взаимодействие с сервером по протоколу HTTP

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -44,4 +44,12 @@ public class ApplicationManager {
   public void stop() {
     wd.quit();
   }
+
+  public HttpSession newSession() {
+    return new HttpSession(this);
+  }
+
+  public Object getProperty(String key) {
+  return properties.getProperty(key);
+  }
 }
