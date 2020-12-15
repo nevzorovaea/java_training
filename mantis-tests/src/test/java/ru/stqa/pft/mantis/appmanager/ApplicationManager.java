2599931e4816c54a44fa15e90e14531e18eefc98
nevzorovaea <nevzorovaea7@gmail.com>
2020-12-15 12:33:45 +0300

    Передача файлов на удалённую машину по протоколу FTP

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -18,6 +18,7 @@ public class ApplicationManager {
   private String browser;
   private WebDriver wd;
   private RegistrationHelper registrationHelper;
+  private FtpHelper ftp;
 
 
   public ApplicationManager(String browser) {
@@ -40,7 +41,7 @@ public class ApplicationManager {
     return new HttpSession(this);
   }
 
-  public Object getProperty(String key) {
+  public String getProperty(String key) {
     return properties.getProperty(key);
   }
 
@@ -50,6 +51,13 @@ public class ApplicationManager {
     }
     return registrationHelper;
   }
+  public FtpHelper ftp() {
+    if (ftp == null) {
+      ftp = new FtpHelper(this);
+    }
+    return ftp;
+  }
+
   public WebDriver getDriver() {
     if (wd == null) {
       if (browser.equals(BrowserType.FIREFOX)) {
