a241c79f9a1b0bc8a71c394dc5fc3076744f9597
nevzorovaea <nevzorovaea7@gmail.com>
2020-12-26 17:12:34 +0300

    Реализована возможность удаленного запуска браузера

--- tests-web-addressbook/src/test/java/appmanager/ApplicationManager.java
+++ tests-web-addressbook/src/test/java/appmanager/ApplicationManager.java
@@ -1,15 +1,19 @@
 package appmanager;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
@@ -28,49 +32,59 @@ public class ApplicationManager {
     this.browser = browser;
     properties = new Properties();
   }
-  public void init() throws IOException  {
+
+  public void init() throws IOException {
     String target = System.getProperty("target", "local");
     properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
 
     dbHelper = new DbHelper();
 
-    if (browser.equals(BrowserType.FIREFOX)) {
-      wd = new FirefoxDriver();
-    } else if (browser.equals(BrowserType.CHROME)) {
-      wd = new ChromeDriver();
-    } else if (browser.equals(BrowserType.IE)) {
-      wd = new InternetExplorerDriver();
+    if ("".equals(properties.getProperty("selenium.server"))) {
+
+      if (browser.equals(BrowserType.FIREFOX)) {
+        wd = new FirefoxDriver();
+      } else if (browser.equals(BrowserType.CHROME)) {
+        wd = new ChromeDriver();
+      } else if (browser.equals(BrowserType.IE)) {
+        wd = new InternetExplorerDriver();
+      }
+    } else {
+      DesiredCapabilities capabilities = new DesiredCapabilities();
+      capabilities.setBrowserName(browser);
+      wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities);
     }
-    wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
-    wd.get(properties.getProperty("web.baseUrl"));
-    groupHelper = new GroupHelper(wd);
-    contactHelper = new ContactHelper(wd);
-    navigationHelper = new NavigationHelper(wd);
-    sessionHelper = new SessionHelper(wd);
-    sessionHelper.login(properties.getProperty("web.adminLogin"), properties.getProperty("web.adminPassword"));
+      wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+      wd.get(properties.getProperty("web.baseUrl"));
+      groupHelper = new GroupHelper(wd);
+      contactHelper = new ContactHelper(wd);
+      navigationHelper = new NavigationHelper(wd);
+      sessionHelper = new SessionHelper(wd);
+      sessionHelper.login(properties.getProperty("web.adminLogin"), properties.getProperty("web.adminPassword"));
+    }
+
+    public void stop () {
+      wd.quit();
+    }
+
+
+    public void returnToGroupPage () {
+      navigationHelper.groupPage();
+    }
+
+    public GroupHelper group () {
+      return groupHelper;
+    }
+
+    public NavigationHelper goTo () {
+      return navigationHelper;
+    }
+
+    public ContactHelper contact () {
+      return contactHelper;
+    }
+
+    public DbHelper db () {
+      return dbHelper;
+    }
+
   }
-
-  public void stop() {
-    wd.quit();
-  }
-
-
-  public void returnToGroupPage() {
-    navigationHelper.groupPage();
-  }
-
-  public GroupHelper group() {
-    return groupHelper;
-  }
-
-  public NavigationHelper goTo() {
-    return navigationHelper;
-  }
-
-  public ContactHelper contact() {
-    return contactHelper;
-  }
-
-  public DbHelper db(){ return dbHelper; }
-
-}
